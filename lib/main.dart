c0886dc840347692e672154cac3b4e6db3743fe0
devesh297 <deveshhgoswamii@gmail.com>
2023-12-12 23:46:44 +0530

    Changes to be committed: 	new file:   .vscode/launch.json 	new file:   assets/images/Login.png 	modified:   ios/Flutter/Debug.xcconfig 	modified:   ios/Flutter/Release.xcconfig 	new file:   ios/Podfile 	new file:   ios/Podfile.lock 	modified:   ios/Runner.xcodeproj/project.pbxproj 	modified:   ios/Runner.xcworkspace/contents.xcworkspacedata 	new file:   lib/Login_page.dart 	new file:   lib/home_page.dart 	modified:   lib/main.dart 	new file:   lib/utlies/routes.dart 	modified:   pubspec.yaml 	modified:   test/widget_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_application_1/Login_page.dart';
+import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/utlies/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
- 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
   
-    return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+     fontFamily: GoogleFonts.lato().fontFamily,),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ) ,
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => LoginPage(),
+        MyRoutes.homeRoutes :(context) => HomePage(),
+        MyRoutes.loginRoutes :(context) => LoginPage(),
+
+      },
     );
   }
 }
